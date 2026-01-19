23b48662e24ff274df755401a30ce87854feb87b
Maverick-GTO <irusanch@gmail.com>
2026-01-19 11:18:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
-const CACHE_NAME = 'vokabel-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './sw.js', 'https://img.icons8.com/ios-filled/512/ffffff/abc-block.png'];
+const CACHE_NAME = 'vokabel-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  'https://img.icons8.com/ios-filled/512/ffffff/abc-block.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
+});
 
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
